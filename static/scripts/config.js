2f41ae9cc9b746c8f3b9419d033508c29a55f4c3
978996836@qq.com <978996836@qq.com>
2022-11-12 17:28:17 +0800

    新增

--- static/scripts/config.js
+++ static/scripts/config.js
@@ -4,6 +4,23 @@ export default {
     "meme/就喜欢看这种.jpg",
     "meme/人妻控.jpg",
     "meme/土味情话.jpg",
-    "meme/渣女幻想.jpg"
+    "meme/渣女幻想.jpg",
+    "meme/帮朋友问问.jpg",
+    "meme/被玩坏了.jpg",
+    "meme/吃我一拳.jpg",
+    "meme/我是女的.jpg",
+    "meme/小赤佬.jpg",
+    "meme/整一个.jpg",
+    "meme/冲醒了.jpg",
+    "meme/从了我吧.jpg",
+    "meme/对象.jpg",
+    "meme/非人类.jpg",
+    "meme/好像是个屁.jpg",
+    "meme/觉醒嫁给我.jpg",
+    "meme/军工品质.jpg",
+    "meme/哭了.jpg",
+    "meme/赛博对象.jpg",
+    "meme/双性恋.jpg",
+    "meme/天堂.jpg"
   ]
 }
